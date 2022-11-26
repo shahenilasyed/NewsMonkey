485c13c71f66956b2cc54b8347d339b3a70cdc56
Shahenila  Syed <70762873+shahenilasyed@users.noreply.github.com>
2022-11-26 16:56:33 +0530

    Add files via upload

---
+++ News.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import { Newsitem } from "./Newsitem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+
+export class News extends Component {
+  static defaultProps = {
+    pageSize: 5,
+    category: "general",
+  };
+  static propTypes = {
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      page: 1,
+      loading: false,
+    };
+  }
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=In&category=${this.props.category}&apiKey=a924ccb868d74075b36ba73faa570915&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      articles: parseData.articles,
+      totalResults: parseData.totalResults,
+      loading: false,
+    });
+  }
+  handlePreviousClick = async () => {
+    let url = `https:loading.gif'//newsapi.org/v2/top-headlines?country=In&category=${
+      this.props.category
+    }&apiKey=a924ccb868d74075b36ba73faa570915&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parseData = await data.json();
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parseData.articles,
+      loading: false,
+    });
+  };
+  handleNextClick = async () => {
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=In&category=${
+        this.props.category
+      }&apiKey=a924ccb868d74075b36ba73faa570915&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
+      let data = await fetch(url);
+      let parseData = await data.json();
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parseData.articles,
+        loading: false,
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="container my-3">
+        <h2 className="text-center">NewsMonkey - Top Headlines</h2>
+        {this.state.loading && <Spinner />}
+        <div className="row ">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4 _" key={element.url}>
+                <Newsitem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imgUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePreviousClick}
+          >
+            &larr; previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
